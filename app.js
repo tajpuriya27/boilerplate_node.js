69ecaa40c343696aed8e3311cd0ed00382bcb2ec
tajpuriya27 <tajpuriyasunil27@gmail.com>
2024-01-18 09:10:03 +0545

    feat: most required middleware added

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("Hello from app.js");
 });
 
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
 module.exports = app;
---
+++ utils/middleware.js
@@ -0,0 +1,24 @@
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+
+const errorHandler = (error, request, response, next) => {
+  console.log("---------");
+
+  if (error.name === "ValidationError") {
+    return response.status(400).json({ error: error.message });
+  } else if (error.name === "JsonWebTokenError") {
+    return response.status(401).json({ error: error.message });
+  } else if (error.name === "TokenExpiredError") {
+    return response.status(401).json({
+      error: "token expired",
+    });
+  }
+
+  next(error);
+};
+
+module.exports = {
+  unknownEndpoint,
+  errorHandler,
+};
